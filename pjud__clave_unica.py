32c5db59eb22cec664f3ed19adca51f156613ce2
Belen <belen@gearlabs.cl>
2025-04-26 20:21:18 -0400

    Cambio de user-agent
    Se cambió el user-agent de chrome de 115.0.0.0 a 124.0.0.0

--- pjud__clave_unica.py
+++ pjud__clave_unica.py
@@ -30,7 +30,7 @@ def setup_driver():
     # Configuración
     #options.add_argument('--headless=chrome')
     options.add_argument('--window-size=1920,1080')
-    options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36')
+    options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36')
     options.add_argument('--disable-gpu')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-software-rasterizer')
